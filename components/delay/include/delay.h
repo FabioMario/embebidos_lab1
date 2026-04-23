31a0b70ec10b5cfc9242d1b91bda1a322f3eb12b
Franco Pereira <fran@loafy.studio>
2026-04-23 20:23:22 -0300

    Delay component comments

--- components/delay/include/delay.h
+++ components/delay/include/delay.h
@@ -1,5 +1,14 @@
 #pragma once
 #include <stdint.h>
 
+/**
+* Bloquea la ejecución durante el tiempo especificado en milisegundos.
+* @param ms: milisegundos a esperar.
+*/
 void delay_ms(uint32_t ms);
+
+/**
+* Bloquea la ejecución durante el tiempo especificado en segundos.
+* @param s: segundos a esperar.
+*/
 void delay_s(uint32_t s);
